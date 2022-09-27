59c270c597162d0bf67d2e4ce98c15627a8c07e3
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-09-27 20:23:28 -0300

    actually use the body on POST requests

--- Http.lean
+++ Http.lean
@@ -8,22 +8,23 @@ namespace Http
 
 namespace Client
 
-def request (method : Method)  (url : URI) (body : Option String) : IO Response := do
+def request (method : Method) (url : URI) (body : Option String) :
+    IO Response := do
   try
     let headers := Headers.fromList [("Host", url.host)]
     let request := Request.init url method headers body
     let text ← String.fromUTF8Unchecked <$> liftM request.send
     match Response.parse text with
     | Except.ok response => return response
-    | Except.error e => throw <| IO.Error.userError e
+    | Except.error e => throw $ IO.Error.userError e
   catch e =>
-    throw <| IO.Error.userError s!"Request failed: {e}"
+    throw $ IO.Error.userError s!"Request failed: {e}"
 
 def get (url : URI) : IO Response :=
   request Method.GET url none
 
 def post (url : URI) (body : String) : IO Response :=
-  request Method.POST url none
+  request Method.POST url (some body)
 
 end Client
 
